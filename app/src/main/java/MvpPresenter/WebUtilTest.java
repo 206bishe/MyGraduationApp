ceec3df93d1861c00ece9282404675dde6ab53f8
harry <54901351+eighteenZ@users.noreply.github.com>
2021-03-25 22:02:35 +0800

    WebHttp Component Test
    测试Android内置web组件HttpURLConnection 用法

---
+++ app/src/main/java/MvpPresenter/WebUtilTest.java
@@ -0,0 +1,34 @@
+package MvpPresenter;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class WebUtilTest {
+    private HttpURLConnection connection;
+    URL url;
+
+    public WebUtilTest() {
+    }
+
+    private HttpURLConnection getConnection() {
+        try{
+            url = new URL("121.4.187.26");
+            connection = (HttpURLConnection) url.openConnection();
+            initConnection(connection);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private void initConnection(HttpURLConnection connection) throws ProtocolException {
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(8000);
+        connection.setReadTimeout(8000);
+    }
+
+
+}
